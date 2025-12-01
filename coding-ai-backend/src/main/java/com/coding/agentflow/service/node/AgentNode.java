6fb243caf21b9f64a3c8f586a2b81e4e6d0fe514
韦龙 <1410124534@qq.com>
2025-12-01 23:37:26 +0800

    temp branch

--- coding-ai-backend/src/main/java/com/coding/agentflow/service/node/AgentNode.java
+++ coding-ai-backend/src/main/java/com/coding/agentflow/service/node/AgentNode.java
@@ -4,9 +4,15 @@ import com.coding.agentflow.model.enums.NodeTypeEnum;
 import com.coding.agentflow.model.model.Node;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -28,6 +34,7 @@ public class AgentNode extends AbstractNode {
     protected NodeExecutionResult doExecute(Node node, Map<String, Object> context) {
         // 获取配置参数 - 需要支持ToolCall
         String chatModelName = getConfigParamAsString(node, "chatModel", "qwen-plus");
+        String userPrompt = getConfigParamAsString(node, "prompt", "");
         // 知识库相关参数
         List<String> knowledgeBaseIds = getConfigParamAsList(node, "knowledgeBaseIds");
         String embeddingModelName = getConfigParamAsString(node, "embeddingModel", "");
@@ -38,14 +45,21 @@ public class AgentNode extends AbstractNode {
         log.info("执行Agent节点，模型: {}, 知识库: {}, 工具: {}", chatModel, knowledgeBaseIds, tools);
 
         // TODO: 实际调用Agent服务
-        // 构建ChatClient
-        ChatClient chatClient = buildChatClient(this.chatModel, chatModelName, tools);
+        // 补充系统提示词 - 1. 嵌入知识库碎片知识 2. 组合系统提示词&用户提示词
+        String finalUserPrompt = replaceTemplateWithVariable(userPrompt, context);
+        String finalSystemPrompt = queryKnowledgeBaseAndBuildPrompts(knowledgeBaseIds, embeddingModelName, rerankModelName, finalUserPrompt);
+
+        // 构建ChatClient - 工具调用 & 思考内容
+        ChatClient.ChatClientRequestSpec chatRequest = buildChatRequest(this.chatModel, chatModelName, tools, finalSystemPrompt, finalUserPrompt);
+
+
         // Agent通常会进行多轮思考和工具调用
+        String output = chatRequest.call().content();
 
         // 包装返回结果
         Map<String, Object> resultData = new HashMap<>();
         resultData.put("reasoning", "Agent推理过程（待实现）");
-        resultData.put("answer", "Agent执行结果（待实现）");
+        resultData.put("answer", output);
 
         return NodeExecutionResult.success(resultData);
     }
@@ -67,25 +81,53 @@ public class AgentNode extends AbstractNode {
     }
 
     /**
-     * 构建ChatClient
-     *
-     * @param chatModel
-     * @param chatModelName
-     * @param tools
-     * @return
+     * 构建Chat请求
      */
-    private ChatClient buildChatClient(ChatModel chatModel, String chatModelName, List<String> tools) {
-        ChatClient chatClient = ChatClient.builder(chatModel)
-                .defaultOptions(ChatOptions.builder()
-                        .model(chatModelName)
-                        .build())
-                .build();
-        // TODO 1. 补充系统和用户提示词 2. 补充知识库嵌入 3. 补充工具调用 4. 补充思考链逻辑
-        chatClient
+    private ChatClient.ChatClientRequestSpec buildChatRequest(ChatModel chatModel, String chatModelName, List<String> tools, String systemPrompt, String userPrompt) {
+        ChatClient chatClient = ChatClient.builder(chatModel).build();
+
+        List<ToolCallback> toolCallbacks = buildToolCallbacks(tools);
+
+        return chatClient
                 .prompt()
-                .advisors()
-                .toolCallbacks()
-                .stream()
+                .messages(List.of(
+                        new SystemMessage(systemPrompt),
+                        new UserMessage(userPrompt)
+                ))
+                .options(OpenAiChatOptions.builder()
+                        .model(chatModelName)
+                        .internalToolExecutionEnabled(true)
+                        .streamUsage(true)
+                        .parallelToolCalls(true)
+                        .build())
+                .toolCallbacks(toolCallbacks);
+    }
+
+    /**
+     * 替换提示词中的变量
+     */
+    private String replaceTemplateWithVariable(String userPrompt, Map<String, Object> context) {
+        // TODO 实现变量替换逻辑
+        return userPrompt;
+    }
+
+
+    /**
+     * 检索模型知识库并构建系统提示词
+     */
+    private String queryKnowledgeBaseAndBuildPrompts(List<String> knowledgeBaseIds, String embeddingModelName, String rerankModelName, String finalUserPrompt) {
+        // TODO
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * 构建工具回调列表
+     * @param tools 工具名称列表
+     * @return 工具回调列表
+     */
+    private List<ToolCallback> buildToolCallbacks(List<String> tools) {
+        // TODO 构建工具列表
+        return List.of();
     }
 
 }
